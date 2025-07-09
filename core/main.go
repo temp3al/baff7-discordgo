bcc21d44fcd01a9e6910402d0504783c533f2ca6
temp3al <temp3al@proton.me>
2025-07-09 12:27:29 -0600

    status function

--- core/main.go
+++ core/main.go
@@ -9,6 +9,15 @@ var (
 
 func Start(session *discordgo.Session) {
 	Session = session
+	// set status
+	session.UpdateStatusComplex(discordgo.UpdateStatusData{
+		Status: "online", // online, idle, dnd or invisible
+		Activities: []*discordgo.Activity{{
+			Name:  "Visual Studio Code",
+			State: "We be codin'",
+			Type:  discordgo.ActivityTypeGame,
+		}},
+	})
 }
 
 func Stop() {
