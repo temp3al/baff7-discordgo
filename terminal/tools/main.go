530b9fc1e5b838c87b10e8030f210fa11e21640d
temp3al <temp3al@proton.me>
2025-07-12 21:15:59 -0600

    lazy test-fail-embed fix

--- terminal/tools/main.go
+++ terminal/tools/main.go
@@ -2,10 +2,11 @@ package tools
 
 import (
 	"discordgo-bot/terminal"
+	"discordgo-bot/utils"
 	"discordgo-bot/utils/ucolor"
 	"discordgo-bot/utils/uembed"
 	"fmt"
-	"strings"
+	"strconv"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -14,20 +15,27 @@ func init() {
 	terminal.TerminalRegcom(terminal.TerminalCom{
 		Name:        "test-fail-embed",
 		Description: "Test an embed failure message.",
-		Usage:       "(ChannelID) [Reason]",
+		Usage:       "(ChannelID) [IDNumber]",
 		Handle: func(args []string) (bool, error) {
-			motive := "Unknown"
-			if len(args) > 1 {
-				motive = strings.Join(args[1:], " ")
+			// cant execute command with less than 1 argument
+			if len(args) < 1 {
+				return false, nil
 			}
+			channel := utils.GetSliceStr(args, 0, "X")
+			id, _ := strconv.Atoi(utils.GetSliceStr(args, 1, ""))
 			_, err := terminal.Session.ChannelMessageSendComplex(
-				args[0],
+				channel,
 				&discordgo.MessageSend{
-					Embed: uembed.GenerateErrorMessage(motive),
+					Embed: uembed.GenerateErrorMessage(id),
 				},
 			)
 			if err == nil {
-				fmt.Printf("%sMessage proccessed @ %s!%s\n", ucolor.OKCYAN, args[0], ucolor.RESET)
+				fmt.Printf(
+					"%sMessage proccessed @ %s!%s\n",
+					ucolor.OKCYAN,
+					channel,
+					ucolor.RESET,
+				)
 			}
 			return true, err
 		},
--- utils/uembed/main.go
+++ utils/uembed/main.go
@@ -14,12 +14,14 @@ var (
 	}
 )
 
-func GenerateErrorMessage(motive string) *discordgo.MessageEmbed {
+// Generates an error message including non-explicit data of the probable cause.
+func GenerateErrorMessage(id int) *discordgo.MessageEmbed {
+	// todo: add more parameters
+	errcode := fmt.Sprintf("0x0F%02dFFFF", id)
 	newErr := &ErrorMessage
 	newErr.Description = fmt.Sprintf(`
 We're sorry, something went wrong while processing your command...
--# fbstring: %s
 -# code: %s
-`, motive, "0x0FBBC123")
+`, errcode)
 	return newErr
 }
